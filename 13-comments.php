6bf538f62c0c4b74937a4e69b2c243a7801682d7
Romeo Cadaoas <romeo.mail7@gmail.com>
2014-09-30 14:33:23 +0400

    13 - comments

---
+++ 13-comments.php
@@ -0,0 +1,13 @@
+<?php
+	
+	/* Block comment
+		Author: 
+		Date:
+
+	*/
+
+	// Single line comment
+	//Uses echo language construct to output the text "Hello World." to the browser 
+	echo 'Hello World.'; //comment again
+	//echo 'Hello World.'; comment the whole code
+?>
