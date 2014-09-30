6f535e4c7eece4065dfd967e5c5ff49a8858d88f
Romeo Cadaoas <romeo.mail7@gmail.com>
2014-09-30 14:43:52 +0400

    14 - Error Reporting

---
+++ 14-ErrorReporting.php
@@ -0,0 +1,7 @@
+<?php
+	/*In php.ini change the error_reporting=E_ALL to show all the errors in the page.
+	if it is error_reporting=0 the page will be blank and no error message will be shown.*/
+	echo $var = 'Alex'
+
+	$var2 = 'Billy';
+?>
