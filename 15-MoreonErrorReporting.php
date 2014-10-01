1c354700bc32a6d0d011cbe2f673279b78657a70
Romeo Cadaoas <romeo.mail7@gmail.com>
2014-10-01 08:49:27 +0400

    15 - More on Error Reporting

---
+++ 15-MoreonErrorReporting.php
@@ -0,0 +1,10 @@
+<?php
+	
+	//changing error reporting in php.ini from a php file.
+	error_reporting(0);
+	//error_reporting(E_ALL);
+	//ini_set('error_reporting',E_ALL);
+
+	echo "string"
+	$var = 'Billy';
+?>
