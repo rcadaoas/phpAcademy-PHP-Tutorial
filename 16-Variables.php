83450cb6a5900e059a28e217c8c77076c18a6216
Romeo Cadaoas <romeo.mail7@gmail.com>
2014-10-01 12:01:44 +0400

    17 - Concatenation

---
+++ 16-Variables.php
@@ -0,0 +1,11 @@
+<?php
+	//to declare a variable you need to start with a Dollar Sign($)
+	$text = 'Hello World'; //string variable should have a qoutation mark
+	$number = 100; //integer should  not have a qoutation mark
+	$bool = true;
+
+	echo $text;
+	echo $number;
+	echo $bool;
+
+?>
