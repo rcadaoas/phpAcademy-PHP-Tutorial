3a78471e9a7842b706bbd9255117160df87801cf
Romeo Cadaoas <romeo.mail7@gmail.com>
2014-10-02 15:14:45 +0400

    20 - Assignment Operators 21 - Comparison Operators

---
+++ 20-AssignmentOperators.php
@@ -0,0 +1,25 @@
+<?php
+	
+	$number1 = 10;
+
+	$number1 += 2;
+
+	echo $number1;
+
+	$number1 -= 5;
+
+	echo $number1;
+
+	$number1 *= 5;
+
+	echo $number1;
+	$number1 /= 2;
+
+	echo $number1;
+
+	$text = '<br />Hello';
+
+	$text .= ' World';
+
+	echo $text;
+?>
---
+++ 21-ComparisonOperators.php
@@ -0,0 +1,51 @@
+<?php
+	$number1 = 10;
+	$number2 = 9;
+
+	//equal
+	if($number1 == $number2){
+		echo 'Inside';
+	}
+
+	//notequal
+	if($number1 != $number2){
+		echo 'Inside';
+	}
+	if($number1 <> $number2){
+		echo 'Inside';
+	}
+
+	//Greater Than
+	if($number1 > $number2){
+		echo 'Yes';
+	}
+	else
+	{
+		echo 'No';
+	}
+
+	if($number1 >= $number2){
+		echo 'Yes';
+	}
+	else
+	{
+		echo 'No';
+	}
+
+	//Less than
+	if($number1 < $number2){
+		echo 'Yes';
+	}
+	else
+	{
+		echo 'No';
+	}
+
+	if($number1 <= $number2){
+		echo 'Yes';
+	}
+	else
+	{
+		echo 'No';
+	}
+?>
