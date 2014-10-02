1b18a90576c182f5a9819e9a84e59a164b5c8ea0
Romeo Cadaoas <romeo.mail7@gmail.com>
2014-10-02 13:58:00 +0400

    17 - Concatenation

---
+++ 17-Concatenation.php
@@ -0,0 +1,15 @@
+<?php
+	
+	$day = 'Thursday';
+	$date = 02;
+	$year = 2014;
+
+	//concatenation
+	//this is the better way
+	echo 'The date is <strong>'.$day.' '.$date.' '.$year.'</strong>';
+
+	//Easy way
+	echo "<br />The date is $day $date $year";
+
+
+?>
