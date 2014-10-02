4df8ba58e419f9a24fa2f5970ce52ec77daf4551
Romeo Cadaoas <romeo.mail7@gmail.com>
2014-10-02 14:12:21 +0400

    8 - if / if else Statement

---
+++ 18-ififelseStatement.php
@@ -0,0 +1,25 @@
+<?php
+	
+	if(true){
+		echo 'TRUE';
+	}
+	else{
+		echo 'False';
+	}
+
+	if(1 == 0){
+		echo 'TRUE';
+	}
+	else{
+		echo 'False';
+	}
+
+	$text = 'Something';
+	if($text == 'qwe'){
+		echo 'TRUE';
+	}
+	else{
+		echo 'False';
+	}
+
+?>
