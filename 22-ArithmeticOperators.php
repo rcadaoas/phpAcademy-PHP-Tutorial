4d0e65e8e25dd7ade4db38f082d9659f66c9610a
rcadaoas <romeo.mail7@gmail.com>
2014-10-05 21:41:42 +0400

    22 - Arithmetic Operators

---
+++ 22-ArithmeticOperators.php
@@ -0,0 +1,38 @@
+<?php
+	//$sum = 10 + 20;
+
+	// echo $sum;
+
+	//$difference = 10 - 20;
+
+	//echo $difference;
+	
+	//multiple operation
+	// $number1 = 100;
+	// $number2 = 50;
+	// $number3 = 2;
+
+	// $result = ($number1 - $number2) / $number3;
+
+	// echo $result;
+	
+	//modulus
+	// $number1 = 10;
+	// $number2 = 3;
+
+	// $result = $number1%$number2;
+
+	// echo $result;
+
+	//++ increment
+	// $number1 = 10;
+	// $number1++;
+
+	// echo $number1;
+
+	//-- decrement
+	$number1 = 10;
+	$number1--;
+
+	echo $number1;
+?>
