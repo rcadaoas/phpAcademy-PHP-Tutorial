d98994eb26a382b8c0941629c8ceef82d7bdb41e
rcadaoas <romeo.mail7@gmail.com>
2014-10-06 15:21:46 +0400

    28 - switch Statement

---
+++ 28-switchStatement.php
@@ -0,0 +1,20 @@
+<?php
+
+	$number = 30;
+
+	switch($number){
+
+		case 1:
+			echo "One";
+			break;
+		case 2;
+			echo "Two";
+			break;
+		case 3;
+			echo "Three";
+			break;
+		default:
+			echo "Number not found.";
+			break;
+	}
+?>
