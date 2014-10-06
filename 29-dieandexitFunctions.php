dd71a82be66210b15e3e2b57cec8a9d9430b1f18
rcadaoas <romeo.mail7@gmail.com>
2014-10-06 15:35:59 +0400

    29 - die and exit Functions

---
+++ 29-dieandexitFunctions.php
@@ -0,0 +1,11 @@
+<?php
+
+	//echo 'Hello ';
+	//die('Error. Page has ended');
+	//exit();
+	//echo "World";
+
+	mysql_connect('localho','asdada','') or die('Could not connect to database.');
+
+	echo "Connected!";
+?>
