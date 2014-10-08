682efe8785029db2adfc8a1dc02257ad3893a837
Romeo Cadaoas <romeo.mail7@gmail.com>
2014-10-08 15:57:22 +0400

    33 - Global Variables and Functions

---
+++ 33-GlobalVariablesandFunctions.php
@@ -0,0 +1,14 @@
+<?php
+	
+	$user_ip = $_SERVER['REMOTE_ADDR'];
+
+	function echo_ip(){
+		global $user_ip, $anothervar,$var2;
+		$string = 'Your IP address is: '.$user_ip;
+
+		echo $string;
+
+	}
+
+	echo_ip();
+?>
