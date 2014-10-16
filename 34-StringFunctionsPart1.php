5186400b22076b8ff63a57f6c6a9c6e581943387
Romeo Cadaoas <romeo.mail7@gmail.com>
2014-10-16 09:52:21 +0400

    37 - String Functions Part 4

--- 34-StringFunctionsPart1.php
+++ 34-StringFunctionsPart1.php
@@ -30,9 +30,32 @@
 
 	//Reverse string
 	$string4 = 'This is an example string.';
-	$strreverse = str_reverse($string4);
-	echo $strreverse;
+	$strreverse = strrev($string4);
+	//echo $strreverse;
 
+	//Checking the similiraty of text using similar_text function
+	$string_one = 'This is my essay. I\'m going to be talking about php.';
+	$string_two = 'This is my essay. I wiil be talking about the subject php.';
 
+	similar_text($string_one, $string_two,$result);
+	// echo 'The similarity is '.$result;
 
+	//get the length of string 
+	$string5 = 'This is an example string.';
+	$string_length = strlen($string5);
+	// echo $string_length;
+
+	//Trimming Text - trim,ltrim,rtrim
+	$string5 = ' This is an example string. ';
+	$string_trimmed = trim($string5); //remove space on both side
+    $string_rtrimmed = rtrim($string5); //remove space of right side
+    $string_ltrimmed = ltrim($string5); //remove space on left side
+	//echo $string_trimmed;
+
+	//add slashes
+	$string5 = ' This is a <img src="image.jpg"> string. ';
+	$string_slashes = htmlentities(addslashes($string5));
+
+	echo $string_slashes;
+	echo stripslashes($string_slashes);
 ?>
