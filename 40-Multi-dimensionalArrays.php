167c566ac2a868e52dd57017f4cbc966f0e94424
Romeo Cadaoas <romeo.mail7@gmail.com>
2014-10-16 10:30:11 +0400

    40 - Multi-dimensional Arrays

---
+++ 40-Multi-dimensionalArrays.php
@@ -0,0 +1,8 @@
+<?php
+	$food = array('Healthy'=>array('Salad','Vegetables','Pasta') //Multi Dimensional Array
+
+				,'UnHealthy'=>array('Pizza','Ice Cream'));
+	echo $food['Healthy'][0];
+	// print_r($food);
+
+?>
