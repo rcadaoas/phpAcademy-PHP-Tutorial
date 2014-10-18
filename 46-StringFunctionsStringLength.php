d5cfc9fc3f459c2aae79b7547822af914627ba36
rcadaoas <romeo.mail7@gmail.com>
2014-10-18 16:01:29 +0400

    46 - String Functions: String Length

---
+++ 46-StringFunctionsStringLength.php
@@ -0,0 +1,12 @@
+<?php
+	$string = 'Alex';
+
+	$string_length = strlen('How many characters does this have?');
+
+	for ($x=1; $x < $string_length ; $x++) { 
+		echo $x.'<br>';
+	}
+
+	echo $string_length;
+
+?>
