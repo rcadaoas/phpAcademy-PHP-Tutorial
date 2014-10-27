130c08a2d8406d7f9ca27c423da6bd923c4f96e3
Romeo Cadaoas <romeo.mail7@gmail.com>
2014-10-27 13:53:39 +0400

    48 - String Functions: String Position Part 1

---
+++ 48-StringFunctionsStringPosition.php
@@ -0,0 +1,12 @@
+<?php
+
+$find = 'is';
+$find_length = strlen($find);
+$string= 'This is a string, and it is an example';
+
+echo strpos($string,$find,10);
+
+while ($string_position = strpos($string,$find,$offset)){
+    
+}
+?>
